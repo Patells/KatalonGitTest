63456b8baa2e70fe951b72bd15934936a9870eb8
Parth <Parth@192.168.0.12>
2018-08-28 20:27:53 -0700

    New thing added.

---
+++ Test Suites/Test_1.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test_1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-08-28T20:25:29</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5855e91c-8e88-4300-b792-e7f60aa88398</testSuiteGuid>
+   <testCaseLink>
+      <guid>bdbe4b55-db1f-4ae9-9b78-40ee73e23fe2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_1_Tom</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d85b38d6-545d-4ed2-b7c0-a40560b9deef</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_2_Kim</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
