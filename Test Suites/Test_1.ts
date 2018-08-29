04d56158bb9530b0d60d105a947859bf89c13278
parth <pparthr32@gmail.com>
2018-08-29 11:57:53 -0700

    test

--- Test Suites/Test_1.ts
+++ Test Suites/Test_1.ts
@@ -4,7 +4,7 @@
    <name>Test_1</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-08-28T21:28:06</lastRun>
+   <lastRun>2018-08-29T11:57:21</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
